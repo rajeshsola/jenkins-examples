b869394b92d0bd8116c671d2a79b89c13a346a59
rajeshsola <rajeshsola@gmail.com>
2018-11-17 23:32:18 +0530

    banking example

---
+++ banking/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "account.h"
+#include "bank.h"
+
+int main() {
+  Account *ptr = new Account(1001, "abcd", 5000);
+  ptr->debit(800);
+  ptr->credit(1200);
+  std::cout << ptr->balanceEnquiry() << std::endl;
+  delete ptr;
+
+  Banking b1;
+  b1.addAccount(1001,"abcd",5000);
+  b1.displayAll();
+  
+}
