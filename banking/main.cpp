c5177fe81cab65661b5d15378a5a935cc04ca92b
rajeshsola <rajeshsola@gmail.com>
2018-11-19 01:03:23 +0530

    updated banking code

--- banking/main.cpp
+++ banking/main.cpp
@@ -12,6 +12,14 @@ int main() {
 
   Banking b1;
   b1.addAccount(1001,"abcd",5000);
+  b1.addAccount(1002,"abc2",8000);
+  b1.addAccount(1003,"abc3",2000);
+  b1.addAccount(1004,"abc4",1000);
+  b1.addAccount(1005,"abc5",6000);
   b1.displayAll();
-  
+  b1.displayOne(1003);
+  std::cout << b1.findMax() << "\n";
+  std::cout << b1.findMin() << "\n";
+  std::cout << b1.findAverage() << "\n";  
+  return 0;
 }
