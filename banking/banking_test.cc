98a5af98ac254af918717d1a5f00cab21485a2d3
rajeshsola <rajeshsola@gmail.com>
2018-11-19 01:24:14 +0530

    adding test cases

---
+++ banking/banking_test.cc
@@ -0,0 +1,22 @@
+#include <limits.h>
+#include "account.h"
+#include "bank.h"
+#include "gtest/gtest.h"
+namespace {   //anonymous namespace
+  TEST(AccountTest, Constructors) {
+    Account a1(1001,"abcd",5000);
+    EXPECT_EQ(5000, a1.getBalance());
+    EXPECT_EQ(1001, a1.getId());    
+  }
+  TEST(AccountTest, Transactions) {
+    Account a1(1001,"abcd",5000);
+    a1.credit(3000);
+    a1.debit(4500);
+    EXPECT_EQ(3500, a1.getBalance());
+    EXPECT_EQ(1001, a1.getId());    
+  }
+}
+int main(int argc,char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
